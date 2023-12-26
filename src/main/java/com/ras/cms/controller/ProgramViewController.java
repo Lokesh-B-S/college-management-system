2d704c5a660755b2ed8c57335e8c4acc317e55f4
Lokesh <lokesh.bs001@gmail.com>
2023-12-26 18:30:43 +0530

    CMS#26- displaying the pre-collected details such as program,sem etc at top in COURSE and OPENELECTIVE dynamic pages

--- src/main/java/com/ras/cms/controller/ProgramViewController.java
+++ src/main/java/com/ras/cms/controller/ProgramViewController.java
@@ -1,8 +1,11 @@
 package com.ras.cms.controller;
 
+import com.ras.cms.domain.Department;
+import com.ras.cms.domain.DepartmentAndProgramFetch;
 import com.ras.cms.domain.Program;
 import com.ras.cms.repository.ProgramRepository;
 import com.ras.cms.service.department.DepartmentService;
+import com.ras.cms.service.departmentProgramFetch.DepartmentProgramFetchService;
 import com.ras.cms.service.program.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,8 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-@RequestMapping("/admin")
+//@RequestMapping("/admin")
 public class ProgramViewController {
 
     @Autowired
@@ -25,15 +30,27 @@ public class ProgramViewController {
     @Autowired
     DepartmentService departmentService;
 
+    @Autowired
+    DepartmentProgramFetchService departmentProgramFetchService;
 
-    @GetMapping(value="/listPrograms")
-    public String programList(Model model) {
+    @GetMapping(value="/hod/listPrograms")
+    public String programListForHOD(Model model, HttpServletRequest request) {
+
+        DepartmentAndProgramFetch departmentAndProgramFetch = departmentProgramFetchService.processRequest(request);
+        Department dep = departmentAndProgramFetch.getDepartment();
+
+        model.addAttribute("programList", programService.getProgramsByDepartment(dep));
+        return "/programList";
+    }
+
+    @GetMapping(value="/admin/listPrograms")
+    public String programListForPrincipal(Model model) {
         model.addAttribute("programList", programService.findAll());
         return "/programList";
     }
 
 
-    @GetMapping(value={"/editProgram","/editProgram/{id}"})
+    @GetMapping(value={"/admin/editProgram","/admin/editProgram/{id}"})
     public String programEdit(Model model, @PathVariable(required = false, name = "id") Long id) {
         if (null != id) {
             model.addAttribute("program", programService.findOne(id));
@@ -45,14 +62,14 @@ public class ProgramViewController {
         return "/programEdit";
     }
 
-    @PostMapping(value="/editProgram")
+    @PostMapping(value="/admin/editProgram")
     public String programEdit(Model model, Program program) {
         programService.saveProgram(program);
         model.addAttribute("programList", programService.findAll());
         return "/programList";
     }
 
-    @GetMapping(value="/deleteProgram/{id}")
+    @GetMapping(value="/admin/deleteProgram/{id}")
     public String programDelete(Model model, @PathVariable(required = true, name = "id") Long id) {
         programService.deleteProgram(id);
         model.addAttribute("programList", programService.findAll());
